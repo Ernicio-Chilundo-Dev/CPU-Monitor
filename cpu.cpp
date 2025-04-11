1fc690c8e995c739589182d701006031429d53fe
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-04-11 15:18:56 +0200

    projeto terminado , ele elva apenas duas funcaoes sendo o coracao do programa a funcao getcpuusage e a funcao principal a main

--- cpu.cpp
+++ cpu.cpp
@@ -9,19 +9,40 @@ using namespace std;
 void getCPUUsage(){
 
     string line;
-    fstream file("/proc/stat");
+    ifstream file("/proc/stat");
 
     if(file.is_open()){
-        getLine(file, line);
+        getline(file, line);
         file.close();
 
         istringstream stream(line);
         string cpu;
 
         long user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
+
+        // Extraçao dos dados da linha
+        stream>>cpu>>user>>nice>>system>>idle>>iowait>>irq>>softirq>>steal>>guest>>guest_nice;
+
+        long totalTime = user+nice+system+idle+iowait+irq+softirq+steal;
+        long activeTime = user+nice+system+irq+softirq+steal;
+
+        //Calcula o uso da CPU como percentagem
+
+        float usage = (float) activeTime / totalTime * 100;
+
+        //Exibe o uso da CPU
+        cout << "Uso da CPU: "<< usage << "%" <<endl;
+    }else{
+        cerr << "Erro ao abrir o arquivo /proc/stat"<<endl; // Casa o arquivo nao seja aberto corretamente
     }
 }
 
 int main(){
+    while (true)
+    {
+        getCPUUsage(); // Chama a funcao de monitoramento
+        sleep(1); // Espera 1 segundo antes de atualizar
+    }
+    
     return  0;
 }
